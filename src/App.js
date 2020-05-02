4df3eab91dd74075aa720bc627e3c0d6d671b9a5
Eduardo Ayora <eduardoaayora64@gmail.com>
2020-05-02 18:09:01 -0500

    se crea PrimarioComponent para poder levantar los estados, App ya no es una clase sino funcion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
+
+import Primario from './PrimarioComponent';
 
 function App() {
   return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
+    <Primario />
   );
 }
 
