d1b456e9502b2409c9fe295595de65b27d031e84
Eduardo Ayora <eduardoaayora64@gmail.com>
2020-05-04 22:32:39 -0500

    single page aplications part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,59 +1,66 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
   function RenderDish({dish}) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
+      return(
+          <Card>
+              <CardImg top src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+          </Card>
+      );
   }
 
-  function RenderComments({dish}) {
-      if (dish != null){
-          const comments = dish.comments.map((comment) => {
-            return (
-              <li key={comment.id}>{comment.comment}</li>
-            );
-          });
-          return(
-              <div>
-                <h1>Commets</h1>
-                <ul>
-                  {comments}
-                </ul>
-              </div>
-          );
-      }
-      else
-          return(
-              <div></div>
-          );
-
+  function RenderComments({comments}) {
+      return(
+          <div>
+            <h1>Commets</h1>
+            <ul>
+              {comments.map((comment) => {
+                return (
+                  <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>{comment.author}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+      );
   }
 
   const DishDetail = (props) => {
-    return (
-      <div className="container">
-        <div className="row">
-          <div  className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.selectedDish} />
+    if(props.dish != null)
+      return (
+          <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
           </div>
-          <div  className="col-12 col-md-5 m-1">
-              <RenderComments dish={props.selectedDish} />
-          </div>
-        </div>
-      </div>
-    );
+      );
+    else
+      return (
+        <div></div>
+      );
   }
 
 export default DishDetail;
