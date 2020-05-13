5e60c3d27e6a32d893a2819abfeb54dde99aba37
Eduardo Ayora <eduardoaayora64@gmail.com>
2020-05-12 22:22:43 -0500

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Button, Modal, ModalHeader, ModalBody,
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
   function RenderDish({dish}) {
       return(
@@ -59,7 +60,25 @@ class DishDetail extends Component {
     }
 
     render() {
-      if(this.props.dish != null)
+      if (this.props.isLoading) {
+          return(
+              <div className="container">
+                  <div className="row">
+                      <Loading />
+                  </div>
+              </div>
+          );
+      }
+      else if (this.props.errMess) {
+          return(
+              <div className="container">
+                  <div className="row">
+                      <h4>{this.props.errMess}</h4>
+                  </div>
+              </div>
+          );
+      }
+      else if (this.props.dish != null)
         return (
             <div className="container">
               <div className="row">
